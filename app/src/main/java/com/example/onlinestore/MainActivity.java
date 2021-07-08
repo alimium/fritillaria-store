fe5d237d661b7509b2945f85c4f05ce52a193a92
Ali Mehrabi <alimehrabi002@gmail.com>
2021-07-08 21:57:23 +0430

    delete extra test code and prepare for bottom navigation bar

--- app/src/main/java/com/example/onlinestore/MainActivity.java
+++ app/src/main/java/com/example/onlinestore/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this, "Test Test Test", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "Test Test Test Baby Test", Toast.LENGTH_SHORT).show();
+
     }
 }
