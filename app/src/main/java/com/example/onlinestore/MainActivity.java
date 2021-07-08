b260a43795ed5aa2cd8f5167a18424234b586974
Ali Mehrabi <alimehrabi002@gmail.com>
2021-07-08 15:11:32 +0430

    test for ali's branch number 2

--- app/src/main/java/com/example/onlinestore/MainActivity.java
+++ app/src/main/java/com/example/onlinestore/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this, "Test Test Test", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Test Test Test Baby Test", Toast.LENGTH_SHORT).show();
     }
 }
