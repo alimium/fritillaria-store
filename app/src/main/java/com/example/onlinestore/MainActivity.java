abb8094966a99b5607e636d52a742b208e03539d
Ali Mehrabi <alimehrabi002@gmail.com>
2021-07-09 17:37:53 +0430

    fix app crash when switching to profile page on bottom navigation bar. implement other pages as fragments. [NOTE] post page should be considered as full screen fragment.

--- app/src/main/java/com/example/onlinestore/MainActivity.java
+++ app/src/main/java/com/example/onlinestore/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.onlinestore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    NavHostFragment navHostFragment;
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //bottom navigation bar functionality
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_controller);
+        navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
 
     }
 }
---
+++ app/src/main/java/com/example/onlinestore/contents/pages/profilepage/ProfileFragment.java
@@ -0,0 +1,31 @@
+package com.example.onlinestore.contents.pages.profilepage;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.onlinestore.R;
+
+public class ProfileFragment extends Fragment{
+
+    public ProfileFragment() {}
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+}
